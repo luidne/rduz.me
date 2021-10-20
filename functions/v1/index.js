2bcde0a10f853eec1c96979057e14e56a9cdde37
Luídne Mota <luidne@gmail.com>
2021-10-20 11:36:25 -0300

    Recebe URL e o salva com o código reduzido no Firestore.

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const {nanoid} = require("nanoid");
+const admin = require("firebase-admin");
 
 const api = express();
+
+admin.initializeApp();
+const db = admin.firestore();
 
 api.use("/about", (req, res) => {
   res.send("API V1");
 });
 
-api.use("/do", (req, res) => {
+api.use("/do/:url", (req, res) => {
+  const url = req.params.url;
   const idGenerated = nanoid(10);
-  res.send(`Códido do URL reduzida: ${idGenerated}`);
+
+  db.collection("urls").doc(idGenerated).set({
+    id: idGenerated,
+    url: url,
+    date: admin.firestore.FieldValue.serverTimestamp(),
+  }).then(() => {
+    console.log(`URL reduzida: https://hostname/${idGenerated}`);
+    res.send(`URL reduzida: https://hostname/${idGenerated}`);
+  }).catch((error) => {
+    console.log(`Erro ao reduzir URL = ${url}`);
+    res.status(500).send(error);
+  });
 });
 
 module.exports = api;
