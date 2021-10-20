e43af5e05802f456632d105bc119de552f577ed5
Luídne Mota <luidne@gmail.com>
2021-10-20 11:27:49 -0300

    Gera o código único com Nano ID.

--- functions/v1/index.js
+++ functions/v1/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const {nanoid} = require("nanoid");
+
 const api = express();
 
-api.use("/init", (req, res) => {
+api.use("/about", (req, res) => {
   res.send("API V1");
+});
+
+api.use("/do", (req, res) => {
+  const idGenerated = nanoid(10);
+  res.send(`Códido do URL reduzida: ${idGenerated}`);
 });
 
 module.exports = api;
