3549ae91e98dbbab89074f32e0e876aa4c228c7a
Luídne Mota <luidne@gmail.com>
2022-01-11 18:11:22 -0300

    Firebase/Google Analytics adicionado.

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -6,6 +6,20 @@ import "antd/dist/antd.css";
 import Router from "./router";
 import i18n from "./translation";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_IP,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
 const App = () => (
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
